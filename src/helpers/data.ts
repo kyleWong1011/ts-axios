4ed037b5370ec2f173e80fb990e6db43de136037
KyleWang <417940197@qq.com>
2020-06-07 18:03:32 +0800

    feat: 将返回的json字符串类型的数据转换为对象类型

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -3,3 +3,20 @@ import { isPlainObject } from './utils'
 export function transformRequest(data: any): any {
   return isPlainObject(data) ? JSON.stringify(data) : data
 }
+
+/**
+ * 将返回的json字符串类型的数据转换为对象类型
+ * @export
+ * @param {*} data
+ * @returns {*}
+ */
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (error) {
+      // ...
+    }
+  }
+  return data
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => transformResponseData(res))
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -29,6 +29,13 @@ function transformHeaders(config: AxiosRequestConfig): any {
   // 因为 processHeaders里的逻辑是判断header不为非值的时候进行处理的
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+// 将返回数据进行处理
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  console.log('res.data', res.data)
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios
